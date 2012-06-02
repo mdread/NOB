dc2c437a2535ba249048225976e0880593bd3e22
Daniel Camarda <0xcaos@gmail.com>
2012-06-02 17:43:32 +0200

    methods in Constraint refactored to recive an Object value

--- src/main/java/net/caoticode/nob/Constraint.java
+++ src/main/java/net/caoticode/nob/Constraint.java
@@ -2,43 +2,48 @@ package net.caoticode.nob;
 
 public class Constraint {
 	public static enum Restriction{
-		EQ, NE, GT, LT, GE, LE, TRUE, FALSE
+		EQ, NE, GT, LT, GE, LE, TRUE, FALSE, NOOP
 	}
 	private Restriction restriction;
+	private Object value;
 	
-	public Constraint(Restriction restriction) {
+	public Constraint(Object obj, Restriction restriction) {
 		this.restriction = restriction;
+		this.value = obj;
 	}
 	
 	public static Constraint eq(Object obj){
-		return new Constraint(Restriction.EQ);
+		return new Constraint(obj, Restriction.EQ);
 	}
 	
 	public static Constraint ne(Object obj){
-		return new Constraint(Restriction.NE);
+		return new Constraint(obj, Restriction.NE);
 	}
 	
 	public static Constraint gt(Object obj){
-		return new Constraint(Restriction.GT);
+		return new Constraint(obj, Restriction.GT);
 	}
 	
 	public static Constraint lt(Object obj){
-		return new Constraint(Restriction.LT);
+		return new Constraint(obj, Restriction.LT);
 	}
 	
 	public static Constraint ge(Object obj){
-		return new Constraint(Restriction.GE);
+		return new Constraint(obj, Restriction.GE);
 	}
 	
 	public static Constraint le(Object obj){
-		return new Constraint(Restriction.LE);
+		return new Constraint(obj, Restriction.LE);
 	}
 	
-	public static Constraint cons(boolean bool){
-		return new Constraint(bool ? Restriction.TRUE : Restriction.FALSE);
+	public static Constraint cons(Object obj, boolean bool){
+		return new Constraint(obj, bool ? Restriction.TRUE : Restriction.FALSE);
 	}
 	
 	public Restriction getRestriction() {
 		return restriction;
+	}
+	public Object getValue() {
+		return value;
 	}
 }
