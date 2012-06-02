278d60464e38005bbc1e4f08fdc6b4658fc972ef
Daniel Camarda <0xcaos@gmail.com>
2012-06-02 19:20:39 +0200

    added NOOP constraint and eval method that evaluates the constraint against the recived object

--- src/main/java/net/caoticode/nob/Constraint.java
+++ src/main/java/net/caoticode/nob/Constraint.java
@@ -4,6 +4,8 @@ public class Constraint {
 	public static enum Restriction{
 		EQ, NE, GT, LT, GE, LE, TRUE, FALSE, NOOP
 	}
+	public static final Constraint NOOP = new Constraint(null, Restriction.NOOP);
+
 	private Restriction restriction;
 	private Object value;
 	
@@ -43,7 +45,43 @@ public class Constraint {
 	public Restriction getRestriction() {
 		return restriction;
 	}
+	
 	public Object getValue() {
 		return value;
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public boolean eval(Object val){
+		if(restriction == Restriction.NOOP)
+				throw new RuntimeException("can not eval on NOOP Constraint");
+		if(val == null && getValue() == null)
+			return true;
+		if(val == null ^ getValue() == null)
+			return false;
+		
+		switch(restriction){
+			case EQ:
+				return val.equals(getValue());
+			case NE:
+				return !(val.equals(getValue()));
+			case GT:
+				return areComparable(val, getValue()) ? ((Comparable)val).compareTo(getValue()) > 0 : false;
+			case GE:
+				return areComparable(val, getValue()) ? ((Comparable)val).compareTo(getValue()) >= 0 : false;
+			case LT:
+				return areComparable(val, getValue()) ? ((Comparable)val).compareTo(getValue()) < 0 : false;
+			case LE:
+				return areComparable(val, getValue()) ? ((Comparable)val).compareTo(getValue()) <= 0 : false;
+			case TRUE:
+				return true;
+			case FALSE:
+				return false;
+			default:
+				return false;
+		}
+	}
+	
+	private boolean areComparable(Object obj1, Object obj2){
+		return obj1 instanceof Comparable && obj1.getClass().isAssignableFrom(obj2.getClass());
 	}
 }
